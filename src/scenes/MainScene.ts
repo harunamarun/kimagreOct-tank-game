e2062e761da3cfc7c944ad7b69ce8f5650b0ad40
harunamarun <harunamarun@gmail.com>
2020-04-16 20:41:44 +0900

    add course

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -1,13 +1,52 @@
 import Phaser from "phaser";
 
 export default class MainScene extends Phaser.Scene {
+  private blocks?: Phaser.Physics.Arcade.StaticGroup;
+
   constructor() {
     super("main");
   }
 
-  preload() {}
-
-  create() {}
+  preload() {
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("block", "assets/block.png");
+  }
+  create() {
+    /******************** set course ********************/
+    this.add.image(400, 300, "ground");
+    //set blocks
+    this.blocks = this.physics.add.staticGroup();
+    this.blocks.create(200, 120, "block").setScale(2).refreshBody();
+    this.blocks.create(600, 120, "block").setScale(2).refreshBody();
+    this.blocks.create(200, 480, "block").setScale(2).refreshBody();
+    this.blocks.create(600, 480, "block").setScale(2).refreshBody();
+    for (let i = 0; i < 5; i++) {
+      this.blocks
+        .create(0, 0, "block")
+        .setScale(1)
+        .setRandomPosition(200, 150, 400, 300)
+        .refreshBody();
+    }
+    let x: number = 0;
+    let y: number = 0;
+    for (let i = 0; i < 25; i++) {
+      this.blocks.create(x + 16, 16, "block");
+      this.blocks.create(x + 16, 600 - 16, "block");
+      x += 32;
+    }
+    for (let i = 0; i < 18; i++) {
+      this.blocks.create(16, y + 16, "block");
+      this.blocks.create(800 - 16, y + 16, "block");
+      y += 32;
+    }
+    x = 0;
+    y = 0;
+    for (let i = 0; i < 8; i++) {
+      this.blocks.create(x + 300, 120, "block");
+      this.blocks.create(x + 280, 600 - 120, "block");
+      x += 32;
+    }
+  }
 
   update() {}
 }
